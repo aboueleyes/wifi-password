8c94b1624c34c78085c9ecec1fd3c7142ac54ed1
aboueleyes <ibrahimabouelanin@gmail.com>
2021-02-12 06:55:24 +0200

    formt code

--- wifi-password.py
+++ wifi-password.py
@@ -1,43 +1,48 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-import os 
-import argparse
-from rich import print as printf 
-import sys 
-import qrcode 
+import os
 import subprocess
+import sys
+
+import qrcode
 
 
 def run_command(command):
     ''' return the stdout od command'''
-    output, _ = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True).communicate()
+    output, _ = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True).communicate()
     return output.decode("utf-8").rstrip('\r\n')
+
 
 def get_ssid():
     '''get ssid of your wifi ssids'''
 
     try:
-        ssid = run_command(f"nmcli -t -f active,ssid dev wifi | grep '^yes:' | sed 's/^yes://'")
+        ssid = run_command(
+            "nmcli -t -f active,ssid dev wifi | grep '^yes:' | sed 's/^yes://'")
     except:
-        printf ("[Bold][Red] Error check your Network Manager[/Bold][/Red]")
+        print(" Error check your Network Manager")
         sys.exit(1)
 
     return ssid
+
 
 def get_password(ssid):
     ''' get the password of your wifi'''
     get_password_command = f"nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'"
 
     # check sudo previlage
-    password = run_command(f"sudo {get_password_command}") if os.getegid() != 0 else run_command(get_password_command)
-    if password =="" :
-        printf ("[Bold][Red] Error password not found /Bold][/Red]")
+    password = run_command(f"sudo {get_password_command}") if os.getegid(
+    ) != 0 else run_command(get_password_command)
+    if password == "":
+        print(" Error password not found")
         sys.exit(1)
     return password
 
+
 def generate_qr(ssid, password):
     ''' generate qr code'''
-    
+
     qr_text = f"WIFI:T:WPA;S:{ssid};P:{password};;"
     qr = qrcode.QRCode(version=1,
                        error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -46,13 +51,13 @@ def generate_qr(ssid, password):
     qr.add_data(qr_text)
     qr.make()
     qr.print_tty()
-   
+
+
 def main():
     ssid = get_ssid()
     password = get_password(ssid)
-    generate_qr(ssid,password)
+    generate_qr(ssid, password)
 
 
-main()    
-
-   
+if if __name__ == "__main__":
+    main()
